addc421d5c35f58a1e5f5bbff56a7c1a20fb3aa3
Bryan Solis <bsolz@uri.edu>
2021-12-08 16:28:33 -0500

    Add/Remove/Edit Prompts

--- src/testing.py
+++ src/testing.py
@@ -5,20 +5,56 @@ import mysql.connector as mysql
 
 LARGEFONT =("Arial", 45)
 
+# Functions
 def add(indicator):
-    
     if indicator == 1: # If from Player
-        answer = simpledialog.askstring("Input", "Insert: Player name, age, weight, height, total points, total assists, total rebounds, total steals, total blocks, total turnovers, team",
-            parent = Application)
+        answer = simpledialog.askstring("Input", "Insert:\n\nPlayer Name\nAge\nWeight\nHeight\nTotal Points\nTotal Assists\nTotal Rebounds\nTotal Steals\nTotal Blocks\nTotal Turnovers\nTeam\n\nCOMMA SEPARATED")
         print(answer)
     elif indicator == 2: # If from Team
-        print("test")
+        answer = simpledialog.askstring("Input", "Insert:\n\nTeam Name\nWins\nLoses\nLocation\nYear Founded\nTotal Championships\nTeam Conference\nTeam Division\n\nCOMMA SEPARATED")
+        print(answer)
     elif indicator == 3: # If from Division
-        print("test")
+        answer = simpledialog.askstring("Input", "Insert:\n\nDivision Name\nDivison Wins\nDivision Losses\nDivision Conference\n\nCOMMA SEPARATED")
+        print(answer)
     elif indicator == 4: # If from Conference
-        print("test")
+        answer = simpledialog.askstring("Input", "Insert:\n\nConference Name\nConference Wins\nConference Losses\nDivision Conference\n\nCOMMA SEPARATED")
+        print(answer)
     else: # If from Draft
-        print("test")
+        answer = simpledialog.askstring("Input", "Insert:\n\nDraft Pick Overall\nDraft Pick Round Relative\nDraft Pick Around\nDraft Year\nPlayer Team\nPlayer Age\nPlayer Weight\nPlayer Height\n\nCOMMA SEPARATED")
+        print(answer)
+def remove(indicator):
+    if indicator == 1: # If from Player
+        answer = simpledialog.askstring("Input", "Insert:\n\nPlayer Name\n\nCOMMA SEPARATED")
+        print(answer)
+    elif indicator == 2: # If from Team
+        answer = simpledialog.askstring("Input", "Insert:\n\nTeam Name\n\nCOMMA SEPARATED")
+        print(answer)
+    elif indicator == 3: # If from Division
+        answer = simpledialog.askstring("Input", "Insert:\n\nDivision Name\n\nCOMMA SEPARATED")
+        print(answer)
+    elif indicator == 4: # If from Conference
+        answer = simpledialog.askstring("Input", "Insert:\n\nConference Name\n\nCOMMA SEPARATED")
+        print(answer)
+    else: # If from Draft
+        answer = simpledialog.askstring("Input", "Insert:\n\nDraft Pick Overall\n\nCOMMA SEPARATED")
+        print(answer)
+
+def edit(indicator):
+    if indicator == 1: # If from Player
+        answer = simpledialog.askstring("Input", "Insert:\n\nPlayer Name\nDesired Attribute to Edit\nNew Value of Attribute\n\nCOMMA SEPARATED")
+        print(answer)
+    elif indicator == 2: # If from Team
+        answer = simpledialog.askstring("Input", "Insert:\n\nTeam Name\nDesired Attribute to Edit\nNew Value of Attribute\n\nCOMMA SEPARATED")
+        print(answer)
+    elif indicator == 3: # If from Division
+        answer = simpledialog.askstring("Input", "Insert:\n\nDivision Name\nDesired Attribute to Edit\nNew Value of Attribute\n\nCOMMA SEPARATED")
+        print(answer)
+    elif indicator == 4: # If from Conference
+        answer = simpledialog.askstring("Input", "Insert:\n\nConference Name\nDesired Attribute to Edit\nNew Value of Attribute\n\nCOMMA SEPARATED")
+        print(answer)
+    else: # If from Draft
+        answer = simpledialog.askstring("Input", "Insert:\n\nDraft Pick Overall\nDesired Attribute to Edit\nNew Value of Attribute\n\nCOMMA SEPARATED")
+        print(answer)
 
 def submit(queryName):
     query = queryName.get()
@@ -55,8 +91,8 @@ class Application(tk.Tk):
         frame = self.frames[cont]
         frame.tkraise()
   
-# first window frame startpage
-  
+
+# first window frame startpage  
 class Launch(tk.Frame):
     def __init__(self, parent, controller):
 
@@ -73,10 +109,8 @@ class Launch(tk.Frame):
         # Placement
         guest_button.place(relx = 0.55, rely = 0.5)
         admin_button.place(relx = 0.35, rely = 0.5)
-  
-          
-  
-  
+
+
 # Guest page
 class Guest(tk.Frame):
      
@@ -91,16 +125,9 @@ class Guest(tk.Frame):
         queryEntrySpace = tk.Entry(self, textvariable = queryName, width = 35)
         queryEntrySpace.place(x = 280, y = 75, width = 200)
 
-        submitbtn = tk.Button(self, text = "Submit", command = submit(queryName))
+        submitbtn = tk.Button(self, text = "Submit", command = lambda: submit(queryName))
         submitbtn.place(x = 340, y = 110, width = 75)
 
-
-        
-
-
-  
-  
-  
   
 # Admin Page
 class Admin(tk.Frame):
@@ -131,20 +158,19 @@ class Add(tk.Frame):
         
         # Buttons
         player_button = tk.Button(self, text = "Player", bg = 'red', width = 5, height = 2, 
-        command = lambda : controller.show_frame(Guest))
+        command = lambda : add(1))
 
         team_button = tk.Button(self, text = "Team", bg = 'red', width = 5, height = 2, 
-        command = lambda : controller.show_frame(Guest))
+        command = lambda : add(2))
 
         division_button = tk.Button(self, text = "Division", bg = 'red', width = 5, height = 2, 
-        command = lambda : controller.show_frame(Guest))
+        command = lambda : add(3))
 
         conference_button = tk.Button(self, text = "Conference", bg = 'red', width = 5, height = 2, 
-        command = lambda : controller.show_frame(Guest))
+        command = lambda : add(4))
     
         draft_player_button = tk.Button(self, text = "Draft Player", bg = 'red', width = 5, height = 2, 
-        command = lambda : controller.show_frame(Guest))
-        
+        command = lambda : add(5))
         
         # Placement
         player_button.place(relx = 0.15, rely = 0.5)
@@ -162,19 +188,19 @@ class Remove(tk.Frame):
         
         # Buttons
         player_button = tk.Button(self, text = "Player", bg = 'red', width = 5, height = 2, 
-        command = lambda : controller.show_frame(Guest))
+        command = lambda : remove(1))
 
         team_button = tk.Button(self, text = "Team", bg = 'red', width = 5, height = 2, 
-        command = lambda : controller.show_frame(Guest))
+        command = lambda : remove(2))
 
         division_button = tk.Button(self, text = "Division", bg = 'red', width = 5, height = 2, 
-        command = lambda : controller.show_frame(Guest))
+        command = lambda : remove(3))
 
         conference_button = tk.Button(self, text = "Conference", bg = 'red', width = 5, height = 2, 
-        command = lambda : controller.show_frame(Guest))
+        command = lambda : remove(4))
     
         draft_player_button = tk.Button(self, text = "Draft Player", bg = 'red', width = 5, height = 2, 
-        command = lambda : controller.show_frame(Guest))
+        command = lambda : remove(5))
 
         # Placement
         player_button.place(relx = 0.15, rely = 0.5)
@@ -182,6 +208,7 @@ class Remove(tk.Frame):
         division_button.place(relx = 0.45, rely = 0.5)
         conference_button.place(relx = 0.6, rely = 0.5)
         draft_player_button.place(relx = 0.75, rely = 0.5)
+
 
 class Edit(tk.Frame):
      
@@ -191,20 +218,19 @@ class Edit(tk.Frame):
         
         # Buttons
         player_button = tk.Button(self, text = "Player", bg = 'red', width = 5, height = 2, 
-        command = lambda : controller.show_frame(Guest))
+        command = lambda : edit(1))
 
         team_button = tk.Button(self, text = "Team", bg = 'red', width = 5, height = 2, 
-        command = lambda : controller.show_frame(Guest))
+        command = lambda : edit(2))
 
         division_button = tk.Button(self, text = "Division", bg = 'red', width = 5, height = 2, 
-        command = lambda : controller.show_frame(Guest))
+        command = lambda : edit(3))
 
         conference_button = tk.Button(self, text = "Conference", bg = 'red', width = 5, height = 2, 
-        command = lambda : controller.show_frame(Guest))
+        command = lambda : edit(4))
     
         draft_player_button = tk.Button(self, text = "Draft Player", bg = 'red', width = 5, height = 2, 
-        command = lambda : controller.show_frame(Guest))
-        
+        command = lambda : edit(5))
         
         # Placement
         player_button.place(relx = 0.15, rely = 0.5)
