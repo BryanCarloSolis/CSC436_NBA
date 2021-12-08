bae8bdb6c4a6a2ecb57fee0640e74caa03a74a46
Bryan Solis <bsolz@uri.edu>
2021-12-08 10:49:44 -0500

    Adding launch screen

---
+++ src/Login code.py
@@ -0,0 +1,33 @@
+from tkinter import *
+import tkinter as tk
+from tkinter import ttk
+import mysql.connector
+
+
+login = tk.Tk()
+login.geometry("1000x1000")
+login.title("NBA Login")
+  
+ 
+lblfrstrow = tk.Label(login, text ="User - Student or Admin: ", )
+lblfrstrow.place(x = 50, y = 20)
+ 
+Username = tk.Entry(login, width = 35)
+Username.place(x = 150, y = 20, width = 100)
+  
+lblsecrow = tk.Label(login, text ="Password -")
+lblsecrow.place(x = 50, y = 50)
+ 
+password = tk.Entry(login, width = 35)
+password.place(x = 150, y = 50, width = 100)
+ 
+submitbtn = tk.Button(login, text ="Login",
+                      bg ='blue', command = submitact)
+submitbtn.place(x = 150, y = 135, width = 55)
+
+def submit():
+    user = Username.get()
+    password = password.get()
+    login(user, password)
+
+#def login(user, password):
--- src/testing.py
+++ src/testing.py
@@ -1,6 +1,9 @@
-import mysql.connector
+from tkinter import *
+import tkinter as tk
+import mysql.connector as mysql
 
-db = mysql.connector.connect(
+'''
+db = mysql.connect(
     host = "localhost",
     user = "root",
     passwd = "Kobebryant24",
@@ -13,4 +16,26 @@ mycursor.execute("select * from Divisions")
 
 for i in mycursor:
     print(i)
+
+'''
+
+root = Tk()
+root.geometry("600x300")
+root.title("2018-19 NBA Season")
+
+guest_button = tk.Button(root, text = "Guest", bg = 'red', width = 5, height = 2)
+#guest_button.grid(column = 0, row = 0)
+guest_button.place(relx = 0.55, rely = 0.5)
+
+admin_button = tk.Button(root, text = "Admin", bg = 'red', width = 5, height = 2)
+#admin_button.grid(column = 1, row = 0)
+admin_button.place(relx = 0.35, rely = 0.5)
+
+
+root.mainloop()
+
+
+
+#id = Label(root, text = 'Enter ID', font = ('bold', 10))
+#id.place(x = 20, y = 30)
 
