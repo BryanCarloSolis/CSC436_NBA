d92a574565bd58a652ef3315de0e34a74e958b60
Bryan Solis <bsolz@uri.edu>
2021-12-08 12:24:41 -0500

    Going from page

--- src/Login code.py
+++ src/Login code.py
@@ -32,3 +32,15 @@ def submit():
 
 #def login(user, password):
     
+'''
+db = mysql.connect(
+    host = "localhost",
+    user = "root",
+    passwd = "Kobebryant24",
+    database = "NBA"
+)
+mycursor = db.cursor()
+mycursor.execute("select * from Divisions")
+for i in mycursor:
+    print(i)
+'''
--- src/testing.py
+++ src/testing.py
@@ -1,41 +1,89 @@
-from tkinter import *
 import tkinter as tk
+from tkinter import ttk
 import mysql.connector as mysql
 
-'''
-db = mysql.connect(
-    host = "localhost",
-    user = "root",
-    passwd = "Kobebryant24",
-    database = "NBA"
-)
+LARGEFONT =("Arial", 45)
 
-mycursor = db.cursor()
+class Application(tk.Tk):
+     
+    def __init__(self, *args, **kwargs):
 
-mycursor.execute("select * from Divisions")
+        tk.Tk.__init__(self, *args, **kwargs)
 
-for i in mycursor:
-    print(i)
+        # creating a container
+        frame = tk.Frame(self) 
+        frame.pack(side = "bottom", fill = "both", expand = True)
+  
+        # initializing frames to an empty array
+        self.frames = {} 
+  
+        # iterate through all the pages
+        for pages in (Launch, Guest, Admin):
+  
+            page = pages(frame, self)
+            page.configure(width=800, height=500)
+            self.frames[pages] = page
 
-'''
+            page.grid(row = 2, column = 2, sticky ="nsew")
 
-root = Tk()
-root.geometry("600x300")
-root.title("2018-19 NBA Season")
+        self.show_frame(Launch)
+  
+    # to display the current frame passed as
+    # parameter
+    def show_frame(self, cont):
+        frame = self.frames[cont]
+        frame.tkraise()
+  
+# first window frame startpage
+  
+class Launch(tk.Frame):
+    def __init__(self, parent, controller):
 
-guest_button = tk.Button(root, text = "Guest", bg = 'red', width = 5, height = 2)
-#guest_button.grid(column = 0, row = 0)
-guest_button.place(relx = 0.55, rely = 0.5)
+        tk.Frame.__init__(self, parent)
 
-admin_button = tk.Button(root, text = "Admin", bg = 'red', width = 5, height = 2)
-#admin_button.grid(column = 1, row = 0)
-admin_button.place(relx = 0.35, rely = 0.5)
+        guest_button = tk.Button(self, text = "Guest", bg = 'red', width = 5, height = 2, 
+        command = lambda : controller.show_frame(Guest))
+        #guest_button.grid(column = 0, row = 0)
+        guest_button.place(relx = 0.55, rely = 0.5)
 
+        admin_button = tk.Button(self, text = "Admin", bg = 'red', width = 5, height = 2,
+        command = lambda : controller.show_frame(Admin))
+        #admin_button.grid(column = 1, row = 0)
+        admin_button.place(relx = 0.35, rely = 0.5)
+  
+          
+  
+  
+# Guest page
+class Guest(tk.Frame):
+     
+    def __init__(self, parent, controller):
 
-root.mainloop()
+        tk.Frame.__init__(self, parent)
+        
+        guest_button = tk.Button(self, text = "Guest", bg = 'red', width = 5, height = 2, 
+        command = lambda : controller.show_frame(Guest))
+        #guest_button.grid(column = 0, row = 0)
+        guest_button.place(relx = 0.55, rely = 0.5)
 
+  
+  
+  
+  
+# Admin Page
+class Admin(tk.Frame):
+    def __init__(self, parent, controller):
+        
+        tk.Frame.__init__(self, parent)
 
+        admin_button = tk.Button(self, text = "Admin", bg = 'red', width = 5, height = 2,
+        command = lambda : controller.show_frame(Admin))
+        #admin_button.grid(column = 1, row = 0)
+        admin_button.place(relx = 0.35, rely = 0.5)
+  
+  
+# Driver Code
+app = Application()
+app.mainloop()
 
-#id = Label(root, text = 'Enter ID', font = ('bold', 10))
-#id.place(x = 20, y = 30)
 
